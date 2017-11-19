820d2b37905836e3871c78252807faef42c12336
Kevin Duh <kevinduh@cs.jhu.edu>
2017-11-19 10:42:02 -0500

    add install scripts

---
+++ install/install_sockeye_cpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# 1. setup python virtual environment 
+venv=sockeye_cpu # set your virtual enviroment name
+conda create -y -n $venv python=3
+source activate $venv
+
+# 2. clone sockeye NMT as submodule and install
+rootdir="$(readlink -f "$(dirname "$0")/../")"
+cd $rootdir
+git submodule init
+git submodule update
+cd sockeye
+pip install -e '.[optional]'
+
---
+++ install/install_sockeye_gpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# 1. setup python virtual environment 
+venv=sockeye_gpu # set your virtual enviroment name
+conda create -y -n $venv python=3
+source activate $venv
+
+# 2. clone sockeye NMT as submodule and install
+rootdir="$(readlink -f "$(dirname "$0")/../")"
+cd $rootdir
+git submodule init
+git submodule update
+cd sockeye
+pip install mxnet-cu80==0.10.0
+pip install pyaml numpy matplotlib tensorboard
+python setup.py install
+
