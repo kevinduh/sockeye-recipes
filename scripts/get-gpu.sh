c2c81ba56dec5729e7f6da694ffc038ad2acb349
Kevin Duh <kevinduh@cs.jhu.edu>
2018-01-31 16:56:57 -0500

    get-gpu.sh respects NVIDIA_VISIBLE_DEVICES environment variable

--- scripts/get-gpu.sh
+++ scripts/get-gpu.sh
@@ -2,15 +2,25 @@
 
 # query for gpu with 0% utilization
 module load cuda80/toolkit
-freegpu=( $( nvidia-smi --format=noheader,csv --query-gpu=index,utilization.gpu | grep ', 0 %' | awk -F ',' '{print $1}' ) )
 
-numfree=${#freegpu[@]}
+if [ $CUDA_VISIBLE_DEVICES ]; then
+    echo "$CUDA_VISIBLE_DEVICES hre" > ~/tmp.getgpu
+    # if CUDA_VISIBLE_DEVICES is already set, just return 0
+    echo 0
 
-if [ $numfree -gt 0 ]; then
-    # randomly pick one gpu id to return
-    pick=$(expr $RANDOM % $numfree )
-    echo ${freegpu[$pick]}
 else
-    # return -1 if no gpu is free
-    echo -1
+
+    # else, look for a free GPU
+    freegpu=( $( nvidia-smi --format=noheader,csv --query-gpu=index,utilization.gpu | grep ', 0 %' | awk -F ',' '{print $1}' ) )
+
+    numfree=${#freegpu[@]}
+
+    if [ $numfree -gt 0 ]; then
+	# randomly pick one gpu id to return
+	pick=$(expr $RANDOM % $numfree )
+	echo ${freegpu[$pick]}
+    else
+	# return -1 if no gpu is free
+	echo -1
+    fi
 fi
