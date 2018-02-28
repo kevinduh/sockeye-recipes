b972614d58482c1161ceb8668214a2ab734de49d
Xuan Zhang <xuanzhang@jhu.edu>
2018-02-28 08:29:22 -0500

    missing quotation marks

--- auto-tuning/train.sh
+++ auto-tuning/train.sh
@@ -76,9 +76,9 @@ $py_cmd -m sockeye.train -s ${train_bpe}.$src \
 # compute bleu on validation set
 # basic settings
 multibleu=$rootdir/tools/multi-bleu.perl
-resultlog=${model_path}multibleu.valid_bpe.result
+resultlog="${model_path}multibleu.valid_bpe.result"
 # use the checkpoint that has the best params
-output = ${model_path}out.valid_bpe.best
+output = "${model_path}out.valid_bpe.best"
 if [ ! -f $output ]; then
 python -m sockeye.translate --models ${model_path} $device < $valid_bpe.$src > $output
 fi
