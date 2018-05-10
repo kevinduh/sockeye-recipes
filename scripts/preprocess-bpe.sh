e3e806190dac3c495e03fcf3169f4bf815b343e5
Kevin Duh <kevinduh@cs.jhu.edu>
2018-05-09 22:12:32 -0400

    minor typo fix

--- scripts/preprocess-bpe.sh
+++ scripts/preprocess-bpe.sh
@@ -27,7 +27,7 @@ mkdir -p $datadir
 echo `date '+%Y-%m-%d %H:%M:%S'` "- Learning BPE on source and creating vocabulary: $bpe_vocab_src"
 python $subword/learn_bpe.py --input ${train_tok}.$src --output $bpe_vocab_src --symbols $bpe_symbols_src --min-frequency $bpe_minfreq 
 
-echo `date '+%Y-%m-%d %H:%M:%S'` "- Applying BPE, creating: ${train_bpe}.$src, ${valid_bpe}.$src" 
+echo `date '+%Y-%m-%d %H:%M:%S'` "- Applying BPE, creating: ${train_bpe_src}, ${valid_bpe_src}" 
 python $subword/apply_bpe.py --input ${train_tok}.$src --codes $bpe_vocab_src --output $train_bpe_src
 python $subword/apply_bpe.py --input ${valid_tok}.$src --codes $bpe_vocab_src --output $valid_bpe_src
 
@@ -37,7 +37,7 @@ python $subword/apply_bpe.py --input ${valid_tok}.$src --codes $bpe_vocab_src --
 echo `date '+%Y-%m-%d %H:%M:%S'` "- Learning BPE on target and creating vocabulary: $bpe_vocab_trg"
 python $subword/learn_bpe.py --input ${train_tok}.$trg --output $bpe_vocab_trg --symbols $bpe_symbols_trg --min-frequency $bpe_minfreq 
 
-echo `date '+%Y-%m-%d %H:%M:%S'` "- Applying BPE, creating: ${train_bpe}.$trg, ${valid_bpe}.$trg" 
+echo `date '+%Y-%m-%d %H:%M:%S'` "- Applying BPE, creating: ${train_bpe_trg}, ${valid_bpe_trg}" 
 python $subword/apply_bpe.py --input ${train_tok}.$trg --codes $bpe_vocab_trg --output $train_bpe_trg
 python $subword/apply_bpe.py --input ${valid_tok}.$trg --codes $bpe_vocab_trg --output $valid_bpe_trg
 
